e78e67450de55c166e1dcff99d7eb5de261d5a8a
Tevyn Augustin <tevynaugustin@gmail.com>
2023-02-10 14:58:52 +0000

    Update Cart Section :)

--- src/pages/cart/cart-item.jsx
+++ src/pages/cart/cart-item.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 
+
 function CartItem(props) {
     const {id, productName, price, productImage} = props.data;
   return (
     <div className='cartItem'>
         <img src={productImage}/>
         <div className='description'>
+          <div className="productName">
             <p><b>{productName}</b></p>
-            <p>{price}</p>
+          </div>
         </div>
+            <div className="price">
+              <p>{price}</p>
+          </div>
     </div>
   )
 }
--- src/pages/cart/cart.jsx
+++ src/pages/cart/cart.jsx
@@ -1,22 +1,25 @@
 import React, {useContext} from 'react'
 import {PRODUCTS} from '../../products';
 import {ShopContext} from '../../context/shop-context'
+import './cart.css'
 import CartItem from './cart-item'
 
 const Cart = () => {
-  const {cartItems} = useContext(ShopContext)
+  const {cartItems} = useContext(ShopContext);
   return (
     <div className='cart'>
       <div>
         <h1>Your Cart Items</h1>
       </div>
-      <div className='cartItems'>
-        {PRODUCTS.map((product) => {
-          if (cartItems[product.id] !== 0) {
-            return <CartItem data={product}/>
-          }
-        })}
-      </div> 
+      <div className='itemContainer'>
+        <div className='cartItems'>
+          {PRODUCTS.map((product) => {
+            if (cartItems[product.id] !== 0) {
+              return <CartItem data={product}/>
+            }
+          })}
+        </div> 
+      </div>
     </div>
   )
 }
