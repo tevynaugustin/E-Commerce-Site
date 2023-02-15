4dcf4b3e6d126730172e8df9c2e58fe9b327ed7c
Tevyn Augustin <tevynaugustin@gmail.com>
2023-02-15 11:24:40 +0000

    update

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ function App() {
   return (
     <div className="App">
       <ShopContextProvider>
-      <Shop/>
       <Router>
         <Navbar/>
         <Routes>
@@ -19,6 +18,7 @@ function App() {
         </Routes>
       </Router>
       </ShopContextProvider>
+      <Shop/>
     </div>
   );
 }
